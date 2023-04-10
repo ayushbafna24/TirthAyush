ff51b60e86689b9ff53f93e25076e33f9d011779
Ayush Bafna <abafna@businessolver.com>
2023-04-10 23:57:02 +0530

    new changes

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+const { proxy, scriptUrl } = require("rtsp-relay")(app);
+
+const handler = proxy({
+  url: `rtsp://admin:Gurudev123@43.229.227.94:554/cam/realmonitor?channel=1&subtype=1`,
+  // if your RTSP stream need credentials, include them in the URL as above
+  verbose: false,
+  additionalFlags: ["-q", "1"],
+});
+
+// the endpoint our RTSP uses
+app.ws("/api/stream", handler);
+
+// this is an example html page to view the stream
+app.get("/", (req, res) =>
+  res.send(`
+  <canvas id='canvas'></canvas>
+
+  <script src='${scriptUrl}'></script>
+  <style>
+  #canvas{
+    position: absolute;
+    width: 100% !important;
+    height: 100% !important;
+  }
+  body{
+    margin: 0 !important;
+  }
+  </style>
+  <script>
+    loadPlayer({
+      url: 'ws://' + location.host + '/api/stream',
+      canvas: document.getElementById('canvas')
+    });
+  </script>
+`)
+);
+const port = 5000;
+app.listen(port, () => {
+  //server starts listening for any attempts from a client to connect at port: {port}
+  console.log(`Now listening on port ${port}`);
+});
